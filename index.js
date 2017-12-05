91b5a5ea3b1b671b1bc1349e866c8712d318f2f0
Nii Yeboah <yeboah.nii@gmail.com>
2017-12-05 15:37:14 +0000

    Bug Fixes

--- index.js
+++ index.js
@@ -1,139 +1,207 @@
 export class Logo {
-    // var w, h, lw, iw, ih, params1, params2, resizing;
-    constructor(canvas_id) {
-        var logoSize = window.innerWidth
-        if (logoSize > window.innerHeight) logoSize = window.innerHeight
-        canvas.width = logoSize * 0.8;
-        canvas.height = canvas.width;
-        InitParams(canvas.width);
-        context.lineWidth = lw;
-        context.lineJoin = "miter";
-        context.strokeStyle = "#000";
-        context.globalAlpha = 0;
-        canvas.style.margin = ((window.innerHeight - canvas.height) / 2 - 10).toString() + "px auto 0";
-        animateLogo();
+    constructor(element_id = false, width = 420, drawLogo = false) {
+        var canvas;
+        if (element_id) {
+            var el = document.getElementById(element_id);
+            if (el.tagName !== "CANVAS") {
+                canvas = document.createElement("canvas");
+                el.appendChild(canvas);
+            } else canvas = el;
+        } else canvas = document.createElement("canvas");
+        canvas.height = canvas.width = width;
+        this._canvas = canvas;
+        this._context = canvas.getContext("2d");
+        this._context.lineJoin = "miter";
+        this._context.strokeStyle = "#000";
+        this._context.globalAlpha = 0;
+        this._initParams(this._canvas.width);
+        if (drawLogo) this.draw();
     }
 
-    _drawLogo() {
-        context.beginPath();
-        context.moveTo(three4ths(w), h - three4ths(h / 2));
-        context.lineTo(w - seven8ths(w / 2), three4ths(h / 2));
-        context.lineTo(w - seven8ths(w / 2), lw / 2);
-        context.lineTo(w - lw / 2, lw / 2);
-        context.lineTo(w - lw / 2, h - lw / 2);
-        context.lineTo(seven8ths(w / 2) + lw / 2, h - lw / 2);
-        context.stroke();
+    draw() {
+        var w = this._canvas.width,
+            lw = this._lw;
+        this._context.save();
+        this._context.globalAlpha = 1;
 
-        context.beginPath();
-        context.moveTo(w - three4ths(w), three4ths(h / 2));
-        context.lineTo(seven8ths(w / 2), h - three4ths(h / 2));
-        context.lineTo(seven8ths(w / 2), h - lw / 2);
-        context.lineTo(lw / 2, h - lw / 2);
-        context.lineTo(lw / 2, lw / 2);
-        context.lineTo(w - seven8ths(w / 2) - lw / 2, lw / 2);
-        context.stroke();
+        this._context.beginPath();
+        this._context.moveTo(Logo._3_4ths(w), w - Logo._3_4ths(w / 2));
+        this._context.lineTo(w - Logo._7_8ths(w / 2), Logo._3_4ths(w / 2));
+        this._context.lineTo(w - Logo._7_8ths(w / 2), lw / 2);
+        this._context.lineTo(w - lw / 2, lw / 2);
+        this._context.lineTo(w - lw / 2, w - lw / 2);
+        this._context.lineTo(Logo._7_8ths(w / 2) + lw / 2, w - lw / 2);
+        this._context.stroke();
 
-        hideInnerLineEdges();
+        this._context.beginPath();
+        this._context.moveTo(w - Logo._3_4ths(w), Logo._3_4ths(w / 2));
+        this._context.lineTo(Logo._7_8ths(w / 2), w - Logo._3_4ths(w / 2));
+        this._context.lineTo(Logo._7_8ths(w / 2), w - lw / 2);
+        this._context.lineTo(lw / 2, w - lw / 2);
+        this._context.lineTo(lw / 2, lw / 2);
+        this._context.lineTo(w - Logo._7_8ths(w / 2) - lw / 2, lw / 2);
+        this._context.stroke();
+
+        this._context.restore();
+        this._hideInnerLineEdges();
     }
 
-    /*- Clears canvas
-        *- Updates global alpha for the fade effect
-        *- Draw the logo
-        *- Draw updated lines 
-        *- Recursively call until both lines are removed  */
-    _animateLogo() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        if (context.globalAlpha < 1) context.globalAlpha += 0.01;
-        updateAndDrawLine(params1);
-        updateAndDrawLine(params2);
-        if (!(params1.removed || params2.removed)) getAnimationFramePF(animateLogo);
+    /**
+     *- Clears canvas
+     *- Updates global alpha for the fade effect
+     *- Draw the logo
+     *- Draw updated lines 
+     *- Recursively call until both lines are removed  
+     */
+    animate() {
+        this._resetCanvas();
+        if (this._context.globalAlpha < 1) this._context.globalAlpha += 0.01;
+        console.log(this._context.globalAlpha);
+        this._updateAllParam();
+        if (!this._animationComplete()) requestAnimationFrame(() => this.animate());
+        else this._context.globalAlpha = 0;
     }
 
-    _updateAndDrawLine(p) {
-        var l = p.line,
-            s = p.segmentIndex,
-            f = p.f,
-            x1 = l.vectorArray[s].x,
-            y1 = l.vectorArray[s].y,
-            x2 = l.vectorArray[s + 1].x,
-            y2 = l.vectorArray[s + 1].y;
-    
-        // Interpolation Formula
-        p.line.currX = Math.round(x1 + (x2 - x1) * f);
-        p.line.currY = Math.round(y1 + (y2 - y1) * f);
+    setWidth(w) {
+        this._canvas.height = this._canvas.width = w;
+        this._initParams(this._canvas.width);
+    }
 
-        drawLine(p.line, p.segmentIndex);
-        hideInnerLineEdges();
-        
-        if (f < 1) p.f += 0.05;
-        else {
-            p.f = 0;
-            if (s + 2 <= p.line.segmentCount) p.segmentIndex++;
-            else p.removed = true; // base case
+    _resetCanvas() {
+        var w = this._canvas.width;
+        this._context.save();
+        this._context.clearRect(0, 0, w, w);
+        this._context.globalAlpha = 1;
+        this._context.fillStyle = "#FFF";
+        this._context.fillRect(0, 0, w, w);
+        this._context.restore();
+    }
+
+    _animationComplete() {
+        var complete = false;
+        for (let i = 0; i < this._params.length; i++) {
+            if (this._params[i].complete) complete = this._params[i].complete;
         }
+        return complete;
+    }
 
-        if (!p.removed) {
-            context.save();
-            context.globalAlpha = 1;
-            context.beginPath();
-            context.arc(p.line.currX, p.line.currY, lw / 2, 0, 2 * Math.PI, false);
-            context.fill();
-            context.restore();
+    _updateAllParam() {
+        for (let i = 0; i < this._params.length; i++) {
+            this._updateAndDrawLine(this._params[i]);
         }
     }
 
-    /*- Draw Line segments until the segmentIndex  */
+    _updateAndDrawLine(param) { // AnimationParam
+        if (!param.complete) {
+            var l = param.line,
+                s = param.segmentIndex,
+                f = param.f,
+                x1 = l.vectorArray[s].x,
+                y1 = l.vectorArray[s].y,
+                x2 = l.vectorArray[s + 1].x,
+                y2 = l.vectorArray[s + 1].y,
+                IF_fn = (x, y, f) => Math.round(x + (y - x) * f); // Interpolation Formula
+            
+            param.line.currX = IF_fn(x1, x2, f);
+            param.line.currY = IF_fn(y1, y2, f);
+
+            this._drawLine(param.line, param.segmentIndex);
+            this._hideInnerLineEdges();
+
+            if (f < 1) param.f += 0.05;
+            else {
+                param.f = 0;
+                if (s + 2 <= param.line.segmentCount) param.segmentIndex++;
+                else param.complete = true; // base case
+            }
+
+            if (!param.complete) this._drawBrushTip(param.line.currX, param.line.currY);
+            else this._outerBorder();
+        }
+    }
+
+    /**
+     * Draw Line segments until the segmentIndex 
+     */
     _drawLine(line, segmentIndex) {
-        context.beginPath();
-        context.moveTo(line.vectorArray[0].x, line.vectorArray[0].y);
+        this._context.beginPath();
+        this._context.moveTo(line.vectorArray[0].x, line.vectorArray[0].y);
         for (var i = 1; i < segmentIndex + 1; i++) {
-            context.lineTo(line.vectorArray[i].x, line.vectorArray[i].y);
+            this._context.lineTo(line.vectorArray[i].x, line.vectorArray[i].y);
         }
-        context.lineTo(line.currX, line.currY);
-        context.stroke();
+        this._context.lineTo(line.currX, line.currY);
+        this._context.stroke();
     }
 
     _hideInnerLineEdges() {
-        context.save();
-        context.globalAlpha = 1;
-        context.fillStyle = "#FFF";
-        context.fillRect(w - lw - iw, h - lw - ih, iw, ih);
-        context.fillRect(lw, lw, iw, ih);
-        context.restore();
+        var w = this._canvas.width,
+            lw = this._lw,
+            iw = this._iw,
+            ih = this._lh;
+        this._context.save();
+        this._context.globalAlpha = 1;
+        this._context.fillStyle = "#FFF";
+        this._context.fillRect(w - lw - iw, w - lw - ih, iw, ih);
+        this._context.fillRect(lw, lw, iw, ih);
+        this._context.restore();
     }
 
-    /*- Initializes global parameters */
-    _InitParams(width) {
-        w = width,
-        h = w,
-        lw = Math.round(w * 0.08), //-> Line Width
-        iw = Math.round((w - lw * 2) / 2), //-> Inner Rect Width
-        ih = Math.round(three4ths(iw) + (three4ths(iw) * 0.045)); //-> Inner Rect Height
+    _outerBorder() {
+        this._context.beginPath();
+        this._context.moveTo(0, 0);
+        this._context.lineTo(this._canvas.width, 0);
+        this._context.lineTo(this._canvas.width, this._canvas.width);
+        this._context.lineTo(0, this._canvas.width);
+        this._context.lineTo(0, 0);
+        this._context.stroke();
+    }
 
-        var line1 = new Line([
-            new Vector(three4ths(w), h - three4ths(h / 2)),
-            new Vector(w - seven8ths(w / 2), three4ths(h / 2)),
-            new Vector(w - seven8ths(w / 2), lw / 2),
+    _drawBrushTip(x, y) {
+        this._context.save();
+        this._context.globalAlpha = 1;
+        this._context.beginPath();
+        this._context.arc(x, y, this._lw / 2, 0, 2 * Math.PI, false);
+        this._context.fillStyle = "#000";
+        this._context.fill();
+        this._context.restore();
+    }
+
+    /**
+     * Initializes global parameters
+     */
+    _initParams() {
+        var w = this._canvas.width,
+            lw = this._lw = Math.round(w * 0.08), //-> Line Width
+            iw = this._iw = Math.round((w - lw * 2) / 2), //-> Inner Rect Width
+            ih = this._lh = Math.round(Logo._3_4ths(iw) + (Logo._3_4ths(iw) * 0.045)); //-> Inner Rect Height
+        
+        this._context.lineWidth = lw;
+
+        var l1 = new Line([
+            new Vector(Logo._3_4ths(w), w - Logo._3_4ths(w / 2)),
+            new Vector(w - Logo._7_8ths(w / 2), Logo._3_4ths(w / 2)),
+            new Vector(w - Logo._7_8ths(w / 2), lw / 2),
             new Vector(w - lw / 2, lw / 2),
-            new Vector(w - lw / 2, h - lw / 2),
-            new Vector(seven8ths(w / 2) + lw / 2, h - lw / 2)
+            new Vector(w - lw / 2, w - lw / 2),
+            new Vector(Logo._7_8ths(w / 2) + lw / 2, w - lw / 2)
         ]);
 
-        var line2 = new Line([
-            new Vector(w - three4ths(w), three4ths(h / 2)),
-            new Vector(seven8ths(w / 2), h - three4ths(h / 2)),
-            new Vector(seven8ths(w / 2), h - lw / 2),
-            new Vector(lw / 2, h - lw / 2),
+        var l2 = new Line([
+            new Vector(w - Logo._3_4ths(w), Logo._3_4ths(w / 2)),
+            new Vector(Logo._7_8ths(w / 2), w - Logo._3_4ths(w / 2)),
+            new Vector(Logo._7_8ths(w / 2), w - lw / 2),
+            new Vector(lw / 2, w - lw / 2),
             new Vector(lw / 2, lw / 2),
-            new Vector(w - seven8ths(w / 2) - lw / 2, lw / 2)
+            new Vector(w - Logo._7_8ths(w / 2) - lw / 2, lw / 2)
         ]);
 
-        params1 = new ParamArray(line1, 0, 0, false);
-        params2 = new ParamArray(line2, 0, 0, false);
+        this._params = [];
+        this._params.push(new AnimationParam(l1, 0, 0, false));
+        this._params.push(new AnimationParam(l2, 0, 0, false));
     }
 
-    static seven8ths(x) { return Math.round(x * 0.875) }
-    static three4ths(x) { return Math.round(x * 0.75) }
+    static _7_8ths(x) { return Math.round(x * 0.875) }
+    static _3_4ths(x) { return Math.round(x * 0.75) }
 }
 
 export class Line {
@@ -152,11 +220,11 @@ export class Vector {
     }  
 }
 
-export class Params {
-    constructor(l, s, f, r) {
-        this.line = l;          //- int         -> the Line to be animated
+export class AnimationParam {
+    constructor(l, s, f, c) {
+        this.line = l;          //- Line        -> the Line to be animated
         this.segmentIndex = s;  //- int         -> used to iterate through the segments (Vector pairs) in a Line
         this.f = f;             //- float       -> factor f for the interpolation formula used to animate the Line
-        this.removed = r;       //- boolean     -> used to determine if the animation is complete
+        this.complete = c;      //- boolean     -> used to determine if the animation is complete
     }
 }
